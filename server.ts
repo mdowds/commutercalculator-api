30bdc0024b2922c5e6c5fdb3c90b09a2d4b41199
Matt Dowds <matthewd99@gmail.com>
2018-12-15 14:21:52 +0000

    Add journeys endpoint

--- server.ts
+++ server.ts
@@ -45,4 +45,49 @@ server.get('/destinations', (req, res) => {
         });
 });
 
+server.get('/journeys/to/:destination', (req, res) => {
+    console.log('Inbound GET', req.url);
+    const destination = db.collection('stations').doc(req.params.destination);
+    const journeys = destination.collection('journeys');
+
+    Promise.all([destination.get(), journeys.get()])
+        .then(([destinationSnapshot, journeysSnapshot]) => {
+            const destination = destinationSnapshot.data();
+            let journeys = [];
+            journeysSnapshot.forEach(journey => {
+                console.log(journey.data());
+                journeys.push(journey.data());
+            });
+
+            const results = journeys.map(journey => ({
+                directionsUrl: '',
+                journeyTime: journey.time,
+                origin: {
+                    id: journey.origin.sid,
+                    name: journey.origin.name,
+                    position: {
+                        lat: journey.origin.location.latitude,
+                        lng: journey.origin.location.longitude
+                    },
+                    postcode: journey.origin.postcode
+                }
+            }));
+
+            const output = {
+                destination: {
+                    id: destination.sid,
+                    name: destination.name,
+                    postcode: destination.postcode,
+                    position: {
+                        lat: destination.location.latitude,
+                        lng: destination.location.longitude
+                    }
+                },
+                results
+            };
+
+            res.send(output);
+        })
+});
+
 server.listen(5000, () => console.log('Server listening on port 5000'));
